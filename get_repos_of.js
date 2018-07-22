afdff2d05cc4344b7933839c5191c0f7eb4f981f
yogiraj99 <ytambake@gmail.com>
2018-07-22 13:06:29 +0530

    Replaced vars by const

--- get_repos_of.js
+++ get_repos_of.js
@@ -1,8 +1,8 @@
-var GitHub = require('github-api');
-var github = new GitHub({
+const GitHub = require('github-api');
+const github = new GitHub({
     apiUrl: "https://serverName/api/v3",
   });
-var user = github.getUser(process.argv[2]);
+const user = github.getUser(process.argv[2]);
 user.listRepos((err, repos)=>{
     console.log(repos.forEach(repo => {
       console.log(repo.full_name)  
